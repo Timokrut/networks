c0520c0a3b80bdb59bf8d8c76860b6123198a520
Timokrut <potapov.tim14@gmail.com>
2026-04-22 20:55:58 +0300

    additional task

--- lab1/main.py
+++ lab1/main.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 def xor(a, b):
     return [i ^ j for i, j in zip(a, b)]
@@ -14,14 +15,13 @@ def mod2div(dividend, divisor):
 
     return dividend[-(n-1):]
 
-# a(x) = m(x)*x^r + c(x)
 def encode(m, g):
     r = len(g) - 1
     padded = m + [0]*r
     remainder = mod2div(padded, g)
     return m + remainder
 
-def count_Pe(g, k, p, eps):
+def count_Pe(g, k, p, eps, extra_k):
     Ne = 0 # счетчик ошибок декодирования
 
     r = len(g) - 1 # deg(g(x))
@@ -31,7 +31,7 @@ def count_Pe(g, k, p, eps):
 
     for _ in range(N):
         # генерация случайного сообщения
-        m = [random.randint(0,1) for _ in range(k)]
+        m = [random.randint(0,1) for _ in range(extra_k)]
 
         # формирование кодового слова 
         a = encode(m, g)
@@ -61,8 +61,22 @@ k = 4             # длина кодируемой последовательн
 p = 0.05          # вероятность ошибки в канале
 eps = 0.01        # требуемая точность результатов
 
-Pe = count_Pe(g, k, p, eps)
+ls = []
+for extra_k in [k - 1, k, k + 1]:
+    Pe_s = []
+    for p in [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]:
+        Pe = count_Pe(g, k, p, eps, extra_k)
+        Pe_s.append(Pe)
+        print(f"Pe: (p = {p})", Pe)
+    ls.append(Pe_s)
 
-print("Pe:", Pe)
-
-# print(mod2div([1, 0, 1, 0, 0, 0, 0], [1, 0, 1, 1]))
+plt.figure(figsize=(8,5))
+plt.title("Зависимость ")
+plt.xlabel("p")
+plt.ylabel("Pe")
+plt.plot([x/10 for x in range(0, 11)], ls[0], label='l > k')
+plt.plot([x/10 for x in range(0, 11)], ls[1], label='l = k')
+plt.plot([x/10 for x in range(0, 11)], ls[2], label='l < k')
+plt.legend()
+plt.grid(True)
+plt.show()
